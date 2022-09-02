63dea28a713d3412c39ab2e45bea69e70d4e3de2
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-09-02 11:45:47 +0300

    margin open, history orders, spot orders filter

--- index.js
+++ index.js
@@ -450,7 +450,7 @@ route.all("/CopyLeaderRequest", upload.none(), async function (req, res) {
   }
 });
 
-route.post("/getMarginOrders", async function (req, res) {
+route.post("/getClosedMarginOrders", async function (req, res) {
   var api_key_result = req.body.api_key;
   let result = await authFile.apiKeyChecker(api_key_result);
   if (result !== true) {
@@ -459,6 +459,20 @@ route.post("/getMarginOrders", async function (req, res) {
   }
 
   let orders = await MarginOrder.find({ user_id: req.body.user_id , status:1}).exec();
+
+  res.json({ status: "success", data: orders });
+});
+
+
+route.post("/getOpenMarginOrders", async function (req, res) {
+  var api_key_result = req.body.api_key;
+  let result = await authFile.apiKeyChecker(api_key_result);
+  if (result !== true) {
+    res.json({ status: "fail", message: "Forbidden 403" });
+    return;
+  }
+
+  let orders = await MarginOrder.find({ user_id: req.body.user_id , status:0}).exec();
 
   res.json({ status: "success", data: orders });
 });
@@ -864,12 +878,24 @@ route.all("/getOrders", upload.none(), async function (req, res) {
   let result = await authFile.apiKeyChecker(api_key_result);
 
   if (result === true) {
-    let list = await Orders.find({
-      user_id: req.body.user_id,
-      status: "0",
-    })
-      .sort({ createdAt: -1 })
-      .exec();
+    var list = [];
+    if(req.body.filter) {
+      list = await Orders.find({
+        user_id: req.body.user_id,
+        type : req.body.filter,
+        status: "0",
+      })
+        .sort({ createdAt: -1 })
+        .exec();
+    } else {
+      list = await Orders.find({
+        user_id: req.body.user_id,
+        status: "0",
+      })
+        .sort({ createdAt: -1 })
+        .exec();
+    }
+    
     res.json({ status: "success", data: list });
   } else {
     res.json({ status: "fail", message: "Forbidden 403" });
