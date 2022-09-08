d27b1438e56f25f2d7bf4af6e18417232718b346
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-09-08 11:37:11 +0300

    ok

--- socket_data.js
+++ socket_data.js
@@ -26,7 +26,7 @@ wss.on("connection", async (ws) => {
       ws.on('message', (data) => {
          console.log("MSG");
          console.log(data.toString());
-         console.log(json.parse(data));
+         console.log(JSON.parse(data));
          let json = JSON.parse(data);
          switch (json.type) {
             case "margin_orders":
