0c599f2fb26cad5d5ada1041895ce31760257992
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-09-20 15:15:52 +0300

    ok

--- Connection.js
+++ Connection.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 async function connection() {
-
+    await mongoose.connect("mongodb://" +
+    process.env.DOCUMENT_DB_UID
+    + ":" +
+    process.env.DOCUMENT_DB_PASS
+    + "@13.59.10.128:27017/?retryWrites=true&w=majority");
+    console.log("Connected");
+    return;
     var mongodbPass = process.env.MONGO_DB_PASS;
     if (process.env.NODE_ENV == 'development') {
         await mongoose.connect("mongodb+srv://volkansaka:" +
@@ -11,7 +17,7 @@ async function connection() {
             process.env.DOCUMENT_DB_UID
             + ":" +
             process.env.DOCUMENT_DB_PASS
-            + "@docdb-2022-09-14-11-39-54.cluster-cx5obo2dvutj.us-east-2.docdb.amazonaws.com:27017/test?replicaSet=rs0&retryWrites=false&w=majority", {
+            + "@docdb-2022-09-14-11-39-54.cluster-cx5obo2dvutj.us-east-2.docdb.amazonaws.com:27017/test?replicaSet=rs0&retryWrites=true&w=majority", {
             ssl: true,
             sslValidate: false,
             sslCA: "./rds-combined-ca-bundle.pem",
