539bef8b22a7ee19198750d064b5ecb5e2981962
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-10-15 15:12:30 +0300

    ok

--- margin_isolated_cron.js
+++ margin_isolated_cron.js
@@ -94,20 +94,139 @@ async function initialize() {
                                 }
                             }
                         } else {
-                            if (order.type == 'buy') {
-                                if (price <= order.target_price) {
-                                    order.method = 'market';
-                                    order.status = 0;
-                                    await order.save();
-                                }
-                            } else if (order.type == 'sell') {
-                                if (price >= order.target_price) {
 
-                                    order.method = 'market';
-                                    order.status = 0;
-                                    await order.save();
-                                }
-                            }
+
+              if (order.type == 'buy') {
+                if (price >= order.target_price) {
+                  continue;
+                }
+              } else if (order.type == 'sell') {
+                if (price <= order.target_price) {
+                  continue;
+                }
+              }
+
+              let reverseOreders = await MarginOrder.findOne({
+                user_id: order.user_id,
+                pair_id: order.pair_id,
+                margin_type: order.margin_type,
+                method: 'market',
+                status: 0,
+              });
+
+              if (reverseOreders) {
+                console.log("reverse order find");
+
+                if (reverseOreders.type == order.type) {
+                  let oldAmount = reverseOreders.amount;
+                  let oldUsedUSDT = reverseOreders.usedUSDT;
+                  let oldPNL = reverseOreders.pnl;
+                  let oldLeverage = reverseOreders.leverage;
+                  let newUsedUSDT = oldUsedUSDT + order.usedUSDT + oldPNL;
+                  
+                  reverseOreders.usedUSDT = newUsedUSDT;
+                  reverseOreders.open_price = price;
+                  reverseOreders.pnl = 0;
+                  reverseOreders.leverage = order.leverage;
+                  reverseOreders.open_time = Date.now();
+                  reverseOreders.amount = (newUsedUSDT * order.leverage) / price;
+
+                  await reverseOreders.save();
+                  await order.remove();
+
+                } else {
+                  //Tersine ise
+                  let checkusdt = (reverseOreders.usedUSDT + reverseOreders.pnl) * reverseOreders.leverage;
+                  if (checkusdt == order.usedUSDT * order.leverage) {
+                    console.log("Bura 0");
+                    reverseOreders.status = 1;
+                    let userBalance = await Wallet.findOne({
+                      coin_id: MarginWalletId,
+                      user_id: req.body.user_id,
+                    }).exec();
+                    userBalance.amount =
+                      userBalance.amount +
+                      reverseOreders.pnl +
+                      reverseOreders.usedUSDT;
+                    await userBalance.save();
+                    await reverseOreders.save();
+                    await order.remove();
+                  }
+
+                  else if (checkusdt > order.usedUSDT * order.leverage) {
+                    console.log("Bura 1");
+                    let writeUsedUSDT =
+                      reverseOreders.usedUSDT + reverseOreders.pnl - order.usedUSDT;
+                    if (writeUsedUSDT < 0) writeUsedUSDT *= -1;
+                    reverseOreders.usedUSDT = writeUsedUSDT;
+                    reverseOreders.amount =
+                      (writeUsedUSDT * reverseOreders.leverage) / price;
+                    let userBalance = await Wallet.findOne({
+                      coin_id: MarginWalletId,
+                      user_id: order.user_id,
+                    }).exec();
+                    userBalance.amount = userBalance.amount + reverseOreders.usedUSDT + order.usedUSDT;
+                    await userBalance.save();
+                    await reverseOreders.save();
+                    await order.remove();
+                  } else {
+                    console.log("Bura 2");
+                    let ilkIslem = reverseOreders.usedUSDT;
+                    let tersIslem = order.usedUSDT;
+                    let data = ilkIslem - tersIslem;
+                    console.log("DATASSS : ", data);
+                    userBalance = await Wallet.findOne({
+                      coin_id: MarginWalletId,
+                      user_id: order.user_id,
+                    }).exec();
+                    userBalance.amount = userBalance.amount + data;
+                    await userBalance.save();
+
+                    reverseOreders.type = order.type;
+                    reverseOreders.leverage = order.leverage;
+                    let writeUsedUSDT =
+                      reverseOreders.usedUSDT + reverseOreders.pnl - order.usedUSDT;
+                    if (writeUsedUSDT < 0) writeUsedUSDT *= -1;
+                    reverseOreders.usedUSDT = writeUsedUSDT;
+                    //reverseOreders.amount = ((((reverseOreders.usedUSDT + reverseOreders.pnl) * leverage) - (usedUSDT * leverage)) / price);
+                    reverseOreders.amount = (writeUsedUSDT * order.leverage) / price;
+                    await reverseOreders.save();
+                    await order.remove();
+                  }
+                }
+              } else {
+                console.log("Bura 44");
+                userBalance = await Wallet.findOne({
+                  coin_id: MarginWalletId,
+                  user_id: order.user_id,
+                }).exec();
+                /*
+                userBalance.amount = userBalance.amount - order.usedUSDT;
+                await userBalance.save();
+                */
+                let n_order = new MarginOrder({
+                  pair_id: order.pair_id,
+                  pair_name: order.pair_name,
+                  //type: order.type == 'buy' ? 'sell' : 'buy',
+                  type: order.type,
+                  margin_type: order.margin_type,
+                  method: 'market',
+                  user_id: order.user_id,
+                  usedUSDT: order.usedUSDT,
+                  required_margin: order.usedUSDT,
+                  isolated: order.usedUSDT,
+                  sl: order.sl,
+                  tp: order.tp,
+                  target_price: order.target_price,
+                  leverage: order.leverage,
+                  amount: order.amount,
+                  open_price: price,
+                });
+                await n_order.save();
+                await order.remove();
+              }
+
+
                         }
                     }
                 }
