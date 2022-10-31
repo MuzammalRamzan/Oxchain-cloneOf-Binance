362ece0d0eef7561969560d4632b0fe7a3c7e604
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-10-31 16:59:04 +0300

    fee part 1

--- controllers/bonus/setFeeCredit.js
+++ controllers/bonus/setFeeCredit.js
@@ -1,21 +1,118 @@
+const FeeModel = require("../../models/FeeModel");
 const Referral = require("../../models/Referral");
 const User = require("../../models/User");
 const UserRef = require("../../models/UserRef");
+const Wallet = require("../../models/Wallet");
 
-const setFeeCredit =  async function(user_id, amount) {
-    let getUser = await User.findOne({user_id : user_id});
-    if(getUser == null) {
+const setFeeCredit = async function (user_id, pair_id, amount) {
+    let getUser = await User.findOne({ user_id: user_id });
+    if (getUser == null) {
         return false;
     }
 
-    let getUserRefCode = await UserRef.findOne({user_id : user_id});
-    let getSubReferrals = await Referral.find({reffer : getUserRefCode.refCode}).sort({createdAt: -1});
+    let getUserRefCode = await UserRef.findOne({ user_id: user_id });
+    let parentReferralCounter = 1;
+    let percentAmount = 20;
+    let searchUserId = user_id;
+    while (parentReferralCounter < 4) {
+        let getParentReferral = await Referral.findOne({ user_id: searchUserId });
+        if (getParentReferral == null) {
+            console.log(searchUserId, " bulunamadı");
+            return;
+        }
+        let parentRefCode = getParentReferral.reffer;
+        let getRefferUser = await UserRef.findOne({ refCode: parentRefCode });
+        console.log(getRefferUser);
+
+        let insertAmount = amount * percentAmount / 100.0;
+        let getWallet = await Wallet.findOne({ user_id: getRefferUser.user_id, coin_id: pair_id });
+        console.log(getWallet);
+        getWallet.amount = parseFloat(getWallet.amount) + insertAmount;
+        await getWallet.save();
+        let insertFeeTable = new FeeModel({
+            feeType: "trade",
+            amount: writeAmount,
+            from_user_id: searchUserId,
+            to_from_user_id: getRefferUser.user_id,
+            pair_id: pair_id,
+            status: 1
+        })
+        await insertFeeTable.save();
+        searchUserId = getRefferUser.user_id;
+        percentAmount = percentAmount / 2;
+        return;
+    }
+    let getSubReferrals = await Referral.find({ reffer: getUserRefCode.refCode }).sort({ createdAt: -1 });
     let counter = 1;
-    for(var i = 0; i < getSubReferrals.length; i++) {
-        switch(counter) {
-            case 1 : 
-            
-            break;
+    let writeAmount = 0.0;
+    let wallet = null;
+    let insert = null;
+    for (var i = 0; i < getSubReferrals.length; i++) {
+        let to_user_id = getSubReferrals[i].user_id;
+        switch (counter) {
+            case 1:
+                writeAmount = amount * 20 / 100.0;
+                wallet = await Wallet.find({ user_id: to_user_id, coin_id: pair_id });
+                wallet.amount = parseFloat(wallet.amount) + writeAmount;
+                await wallet.save();
+                insert = new FeeModel({
+                    feeType: "trade",
+                    amount: writeAmount,
+                    from_user_id: from_user_id,
+                    to_from_user_id: to_user_id,
+                    pair_id: pair_id,
+                    status: 1
+                });
+                await insert.save();
+                break;
+
+            case 2:
+                writeAmount = amount * 10 / 100.0;
+                wallet = await Wallet.find({ user_id: to_user_id, coin_id: pair_id });
+                wallet.amount = parseFloat(wallet.amount) + writeAmount;
+                await wallet.save();
+                insert = await new FeeModel({
+                    feeType: "trade",
+                    amount: writeAmount,
+                    from_user_id: from_user_id,
+                    to_from_user_id: to_user_id,
+                    pair_id: pair_id,
+                    status: 1
+                });
+                await insert.save();
+                break;
+
+            case 3:
+                writeAmount = amount * 5 / 100.0;
+                wallet = await Wallet.find({ user_id: to_user_id, coin_id: pair_id });
+                wallet.amount = parseFloat(wallet.amount) + writeAmount;
+                await wallet.save();
+                insert = await new FeeModel({
+                    feeType: "trade",
+                    amount: writeAmount,
+                    from_user_id: from_user_id,
+                    to_from_user_id: to_user_id,
+                    pair_id: pair_id,
+                    status: 1
+                });
+                await insert.save();
+                break;
+
+            case 4:
+                writeAmount = amount * 5 / 100.0;
+                wallet = await Wallet.find({ user_id: to_user_id, coin_id: pair_id });
+                wallet.amount = parseFloat(wallet.amount) + writeAmount;
+                await wallet.save();
+                insert = await new FeeModel({
+                    feeType: "trade",
+                    amount: writeAmount,
+                    from_user_id: from_user_id,
+                    to_from_user_id: to_user_id,
+                    pair_id: pair_id,
+                    status: 1
+                });
+                await insert.save();
+                break;
         }
     }
 }
--- models/FeeModel.js
+++ models/FeeModel.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const FeeModelSchema = new mongoose.Schema({
   feeType: { type: String, required: true },
   amount: { type: Number, required: true },
-  user_id: { type: mongoose.Schema.Types.ObjectId, required: true , ref : "User"},
+  from_user_id: { type: mongoose.Schema.Types.ObjectId, required: true , ref : "User"},
+  to_user_id: { type: mongoose.Schema.Types.ObjectId, required: true , ref : "User"},
   pair_id: { type: mongoose.Schema.Types.ObjectId, required: true , ref : "Pairs"},
   status: { type: String, required: false, default: 0 },
   createdAt: {
