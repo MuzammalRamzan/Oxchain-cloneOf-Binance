43c3c71587313fb0509b532e0b3382476a2054f4
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-11-02 17:39:48 +0300

    delete spot limit

--- controllers/orders/deleteLimit.js
+++ controllers/orders/deleteLimit.js
@@ -1,53 +1,71 @@
 const Wallet = require("../../models/Wallet");
 const Pairs = require("../../models/Pairs");
 const Orders = require("../../models/Orders");
-const MarginOrder = require("../../models/MarginOrder");
 
-const MarginWalletId = "62ff3c742bebf06a81be98fd";
 
 const deleteLimit = async (req, res) => {
-  console.log(req.body.user_id);
-  console.log(req.body.order_id);
   
-  let doc = await MarginOrder.findOneAndUpdate(
-    {
-      _id: req.body.order_id,
-      user_id: req.body.user_id,
-      type: "limit",
-      status: 1,
-    },
-    { $set: { status: -1 } }
-  );
-  console.log(doc);
-  if (doc) {
-    let userBalance = await Wallet.findOne({
-      coin_id: MarginWalletId,
-      user_id: doc.user_id,
-    });
+  let order = await Orders.findOne({
+    _id: req.body.order_id,
+    user_id: req.body.user_id,
+    type: "limit",
+  }).exec();
+  if (order) {
+    let amount = order.amount;
+    let pair = await Pairs.findOne({ symbolOneID: order.pair_id }).exec();
 
-    userBalance.amount = userBalance.amount + doc.usedUSDT;
-    await userBalance.save();
-  }
-  doc = await MarginOrder.findOneAndUpdate(
-    {
-      _id: req.body.order_id,
-      user_id: req.body.user_id,
-      type: "stop_limit",
-      status: 1,
-    },
-    { $set: { status: -1 } }
-  );
-  console.log(doc);
-  if (doc) {
-    let userBalance = await Wallet.findOne({
-      coin_id: MarginWalletId,
-      user_id: doc.user_id,
-    });
-
-    userBalance.amount = userBalance.amount + doc.usedUSDT;
-    await userBalance.save();
+    if (order.method == "buy") {
+      var toWallet = await Wallet.findOne({
+        coin_id: pair.symbolTwoID,
+        user_id: order.user_id,
+      }).exec();
+      toWallet.amount = toWallet.amount + order.target_price * amount;
+      await toWallet.save();
+    } else {
+      var toWallet = await Wallet.findOne({
+        coin_id: pair.symbolOneID,
+        user_id: order.user_id,
+      }).exec();
+      toWallet.amount = toWallet.amount + amount;
+      await toWallet.save();
+    }
   }
 
+  order = await Orders.findOne({
+    _id: req.body.order_id,
+    user_id: req.body.user_id,
+    type: "stop_limit",
+  }).exec();
+
+  if (order) {
+    let amount = order.amount;
+    let pair = await Pairs.findOne({ symbolOneID: order.pair_id }).exec();
+
+    if (order.method == "buy") {
+      var toWallet = await Wallet.findOne({
+        coin_id: pair.symbolTwoID,
+        user_id: order.user_id,
+      }).exec();
+      toWallet.amount = toWallet.amount + order.target_price * amount;
+      await toWallet.save();
+    } else {
+      var toWallet = await Wallet.findOne({
+        coin_id: pair.symbolOneID,
+        user_id: order.user_id,
+      }).exec();
+      toWallet.amount = toWallet.amount + amount;
+      await toWallet.save();
+    }
+  }
+
+  await Orders.findOneAndUpdate(
+    { _id: req.body.order_id, user_id: req.body.user_id, type: "limit" },
+    { $set: { status: -1 } }
+  ).exec();
+  await Orders.findOneAndUpdate(
+    { _id: req.body.order_id, user_id: req.body.user_id, type: "stop_limit" },
+    { $set: { status: -1 } }
+  ).exec();
   res.json({ status: "success", message: "removed" });
 };
 
