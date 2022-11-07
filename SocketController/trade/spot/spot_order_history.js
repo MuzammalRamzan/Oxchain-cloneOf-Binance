d6c01d91e4281b7d7ab2357f8615ba12a39f61f1
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-11-07 14:26:44 +0300

    socket part 3

--- SocketController/trade/spot/spot_order_history.js
+++ SocketController/trade/spot/spot_order_history.js
@@ -2,7 +2,7 @@ const Orders = require("../../../models/Orders");
 
 const SpotOrderHistory = async (ws, user_id) => {
     let orders = await Orders.find({
-        user_id: user_id, type: 'limit', $and:
+        user_id: user_id, type: 'limit', $or:
             [
                 { status: -2 },
                 { status: -1 },
@@ -13,7 +13,7 @@ const SpotOrderHistory = async (ws, user_id) => {
     Orders.watch([{ $match: { operationType: { $in: ['insert', 'update', 'remove', 'delete'] } } }]).on('change', async data => {
         console.log("fa");
         let orders = await Orders.find({
-            user_id: user_id, type: 'limit', $and:
+            user_id: user_id, type: 'limit', $or:
                 [
                     { status: -2 },
                     { status: -1 },
