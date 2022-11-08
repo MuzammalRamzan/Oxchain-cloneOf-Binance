fd951873248d1705defb0a95d3dd63e7420b93fa
furkandamar-hctbilisim <99255244+furkandamar-hctbilisim@users.noreply.github.com>
2022-11-08 12:49:37 +0300

    cross socket

--- SocketController/trade/cross/cross_open_orders.js
+++ SocketController/trade/cross/cross_open_orders.js
@@ -2,11 +2,11 @@ const MarginOrder = require("../../../models/MarginOrder");
 
 const CrossOpenOrders = async (ws, user_id) => {
     
-    let orders = await MarginOrder.find({ user_id: user_id, method: 'market', status: 1 });
+    let orders = await MarginOrder.find({ user_id: user_id, method: 'market', status: 0 });
     ws.send(JSON.stringify({ type: 'open_orders', content: orders }));
 
     MarginOrder.watch([{ $match: { operationType: { $in: ['insert', 'update', 'remove', 'delete'] } } }]).on('change', async data => {
-        let orders = await MarginOrder.find({ user_id: user_id, method: 'market', status: 1 });
+        let orders = await MarginOrder.find({ user_id: user_id, method: 'market', status: 0 });
         ws.send(JSON.stringify({ type: 'open_orders', content: orders }));
     });
 
