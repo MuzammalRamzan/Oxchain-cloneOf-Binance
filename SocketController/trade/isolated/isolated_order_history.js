27382244cf5ce89ff70c3cf19c9197693a728f6b
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-11-10 15:30:34 +0300

    ok

--- SocketController/trade/isolated/isolated_order_history.js
+++ SocketController/trade/isolated/isolated_order_history.js
@@ -3,11 +3,11 @@ const MarginOrder = require("../../../models/MarginOrder");
 const IsolatedOrderHistory = async (ws, user_id) => {
  
     let orders = await MarginOrder.find({ user_id: user_id, margin_type:"isolated", method: "limit" });
-    ws.send(JSON.stringify({page:"isolated", type: 'open_orders', content: orders }));
+    ws.send(JSON.stringify({page:"isolated", type: 'order_history', content: orders }));
 
     MarginOrder.watch([{ $match: { operationType: { $in: ['insert', 'update', 'remove', 'delete'] } } }]).on('change', async data => {
         let orders = await MarginOrder.find({ user_id: user_id, margin_type:"isolated", method: "limit" });
-        ws.send(JSON.stringify({page:"isolated", type: 'open_orders', content: orders }));
+        ws.send(JSON.stringify({page:"isolated", type: 'order_history', content: orders }));
     });
 
 }
