29e0bb499ba15f1a0e248b37e19bc7dab5f939b5
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-11-10 18:01:30 +0300

    ok

--- SocketController/trade/spot/spot_open_orders.js
+++ SocketController/trade/spot/spot_open_orders.js
@@ -1,12 +1,12 @@
 const Orders = require("../../../models/Orders");
 
 const SpotOpenOrders = async (ws, user_id) => {
-    let orders = await Orders.find({ user_id: user_id, type: 'limit', status: 1 });
-    ws.send(JSON.stringify({page:"spot", type: 'open_orders', content: orders }));
+    let orders = await Orders.find({ user_id: user_id, $or: [{ type: 'limit' }, { type: 'stop_limit' },], status: 1 });
+    ws.send(JSON.stringify({ page: "spot", type: 'open_orders', content: orders }));
 
     Orders.watch([{ $match: { operationType: { $in: ['insert', 'update', 'remove', 'delete'] } } }]).on('change', async data => {
-        let orders = await Orders.find({ user_id: user_id, type: 'limit', status: 1 });
-        ws.send(JSON.stringify({ page:"spot", type: 'open_orders', content: orders }));
+        let orders = await Orders.find({ user_id: user_id, $or: [{ type: 'limit' }, { type: 'stop_limit' },], status: 1 });
+        ws.send(JSON.stringify({ page: "spot", type: 'open_orders', content: orders }));
     });
 
 }
