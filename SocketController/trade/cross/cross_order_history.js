0025f545b080bc005aa5ca796a859a3a3e7daf41
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-11-10 11:56:21 +0300

    socket part 10

--- SocketController/trade/cross/cross_order_history.js
+++ SocketController/trade/cross/cross_order_history.js
@@ -3,11 +3,11 @@ const MarginOrder = require("../../../models/MarginOrder");
 const CrossOrderHistory = async (ws, user_id) => {
  
     let orders = await MarginOrder.find({ user_id: user_id, margin_type:"cross", method: "limit" });
-    ws.send(JSON.stringify({page:"cross", type: 'open_orders', content: orders }));
+    ws.send(JSON.stringify({page:"cross", type: 'order_history', content: orders }));
 
     MarginOrder.watch([{ $match: { operationType: { $in: ['insert', 'update', 'remove', 'delete'] } } }]).on('change', async data => {
         let orders = await MarginOrder.find({ user_id: user_id, margin_type:"cross", method: "limit" });
-        ws.send(JSON.stringify({page:"cross", type: 'open_orders', content: orders }));
+        ws.send(JSON.stringify({page:"cross", type: 'order_history', content: orders }));
     });
 
 }
