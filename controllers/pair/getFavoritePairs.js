1096ca80f98ead01bb5319c5f85c8a11ff10783b
Volkansaka <volkansaka@oxhain.com>
2022-12-01 16:34:41 +0300

    update

--- controllers/pair/getFavoritePairs.js
+++ controllers/pair/getFavoritePairs.js
@@ -1,11 +1,24 @@
 const FavoriteCoin = require("../../models/FavoriteCoin");
-const GetFavoritePairs = async(req,res) => {
-    try {
-        let user_id = req.body.user_id;
-        let favs = await FavoriteCoin.find({user_id : user_id});
-        res.json({status : 'success', data : favs});
-    } catch(err) {
-        res.json({status : 'fail', msg : err.message});
+const Pairs = require("../../models/Pairs");
+const CoinList = require("../../models/CoinList");
+
+const GetFavoritePairs = async (req, res) => {
+  try {
+    let user_id = req.body.user_id;
+    let favs = await FavoriteCoin.find({ user_id: user_id });
+
+    let returnData = [];
+    for (let i = 0; i < favs.length; i++) {
+      let coinlist = await CoinList.findOne({ _id: favs[i].coin_id });
+
+      returnData.push({
+        coin_id: favs[i].coin_id,
+        pair_name: coinlist.symbol + "/USDT",
+      });
     }
-}
+    res.json({ status: "success", data: returnData });
+  } catch (err) {
+    res.json({ status: "fail", msg: err.message });
+  }
+};
 module.exports = GetFavoritePairs;
