ab9d42bffa8ce5ff3bb5c2d7342472a54e20671e
Volkansaka <volkansaka@oxhain.com>
2022-12-02 16:10:53 +0300

    update

--- controllers/pair/setFavoritePair.js
+++ controllers/pair/setFavoritePair.js
@@ -1,23 +1,32 @@
 const FavoriteCoin = require("../../models/FavoriteCoin");
+const Pairs = require("../../models/Pairs");
 
 const SetFavoritePair = async (req, res) => {
-    try {
-        let coin_id = req.body.coin_id;
-        let user_id = req.body.user_id;
-        let check = await FavoriteCoin.findOne({ user_id: user_id, coin_id: coin_id });
-        if (check == null) {
-            let save = new FavoriteCoin({
-                user_id : user_id,
-                coin_id : coin_id
-            });
-            await save.save();
-            res.json({ status: 'success', data: 'OK' });
-        } else {
-            await check.remove();
-            res.json({ status: 'success', data: 'OK' });
-        }
-    } catch(err) {
-        res.json({ status: 'fail', msg: err.message });
+  try {
+    let user_id = req.body.user_id;
+    let pair = req.body.pair;
+
+    let pairData = await Pairs.findOne({
+      name: pair,
+    });
+
+    let check = await FavoriteCoin.findOne({
+      user_id: user_id,
+      coin_id: pairData.symbolOneID,
+    });
+    if (check == null) {
+      let save = new FavoriteCoin({
+        user_id: user_id,
+        coin_id: pairData.symbolOneID,
+      });
+      await save.save();
+      res.json({ status: "success", data: "OK" });
+    } else {
+      await check.remove();
+      res.json({ status: "success", data: "OK" });
     }
-}
+  } catch (err) {
+    res.json({ status: "fail", msg: err.message });
+  }
+};
 module.exports = SetFavoritePair;
