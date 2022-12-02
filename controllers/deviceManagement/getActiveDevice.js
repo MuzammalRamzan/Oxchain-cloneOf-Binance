d4d805b5e98402cf796428c60113af85f8b8f64d
Adeel Ahmed <adeel@lessgo.co>
2022-12-02 17:14:57 +0500

    update

--- controllers/deviceManagement/getActiveDevice.js
+++ controllers/deviceManagement/getActiveDevice.js
@@ -4,15 +4,17 @@ var Device = require("../../models/Device");
 
 const getActiveDevice = async function (req, res) {
   var api_key_result = req.body.api_key;
+  const deviceName = req.body.deviceName;
 
   let result = await authFile.apiKeyChecker(api_key_result);
 
   if (result === true) {
-   
-    let device = await Device.find({
-      user_id: req.body.user_id,
-      status: 1,
-    }).exec();
+   const query = {
+    user_id: req.body.user_id,
+    status: 1,
+  }
+  if (deviceName) query.deviceName = deviceName;
+    let device = await Device.find(query).exec();
 
     if (device != null) {
       res.json({ status: "success", data: device });
