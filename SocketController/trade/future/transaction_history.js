7ab1d6760ddf92f9f6326847cce955804e543dfc
furkandamar-hctbilisim <99255244+furkandamar-hctbilisim@users.noreply.github.com>
2022-12-06 14:58:29 +0300

    transaction history socket

--- SocketController/trade/future/transaction_history.js
+++ SocketController/trade/future/transaction_history.js
@@ -1,27 +1,27 @@
 const Transactions = require("../../../models/Transactions");
 
 const FutureTransactionHistory = async (ws, user_id, filter) => {
-    let request = { user_id: user_id};
-   
-    if (filter['type'] == 'all' || filter['type'] == null) {
-        request["type"] = filter['type'];
-    } 
+    let request = { user_id: user_id };
 
-    if ((filter['side'] != null)) {
-        if (filter['side'] != 'all')
-            request['type'] = filter['side'];
+    if (filter['type'] != null) {
+        if (filter['type'] != 'all')
+            request["type"] = filter['type'];
     }
 
-  
-    if(filter['date_from'] != null && filter['date_to'] != null) {
-        request['createdAt'] = {$gte : filter['date_from'], $lt : filter['date_to']};
+
+
+
+    if (filter['date_from'] != null && filter['date_to'] != null) {
+        request['createdAt'] = { $gte: filter['date_from'], $lt: filter['date_to'] };
     }
 
-    let data = await Transactions.find(request);
-    ws.send(JSON.stringify({ page: "future", type: 'transaction_history', content: data }));
-    Transactions.watch([{$match: {operationType : {$in: ['insert', 'update','remove', 'delete']}}}]).on('change', async data => {
-        let data = await Transactions.find(request);
-        ws.send(JSON.stringify({ page: "future", type: 'transaction_history', content: data }));
+    console.log(request);
+    let table = await Transactions.find(request);
+    ws.send(JSON.stringify({ page: "future", type: 'transaction_history', content: table }));
+
+    Transactions.watch([{ $match: { operationType: { $in: ['insert', 'update', 'remove', 'delete'] } } }]).on('change', async data => {
+        let table = await Transactions.find(request);
+        ws.send(JSON.stringify({ page: "future", type: 'transaction_history', content: table }));
     });
 }
 module.exports = FutureTransactionHistory;
