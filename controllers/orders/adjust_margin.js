ed7323193a89c52ced6803bccaafabe4d34a832c
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2022-12-07 19:46:39 +0300

    test files adjust

--- controllers/orders/adjust_margin.js
+++ controllers/orders/adjust_margin.js
@@ -2,56 +2,57 @@ const FutureOrder = require("../../models/FutureOrder");
 const FutureWalletModel = require("../../models/FutureWalletModel");
 
 const AdjustMargin = async (req, res) => {
-    let order_id = req.body.order_id;
-    let user_id = req.body.user_id;
-    let amount = parseFloat(req.body.amount) ?? 0.0;
-    let type = req.body.type;
+  let order_id = req.body.order_id;
+  let user_id = req.body.user_id;
+  let amount = parseFloat(req.body.amount) ?? 0.0;
+  let type = req.body.type;
 
+  let order = await FutureOrder.findOne({
+    user_id: user_id,
+    _id: order_id,
+    method: "market",
+    status: 0,
+  });
 
-    let order = await FutureOrder.findOne({ user_id: user_id, _id: order_id, method: "market", status : 0 });
+  if (order == null) {
+    res.json({ status: "fail", message: "Invalid Order" });
+    return;
+  }
 
-    if (order == null) {
-        res.json({ status: "fail", message: "Invalid Order" });
-        return;
+  if (type == "add") {
+    let wallet = await FutureWalletModel.findOne({ user_id: user_id });
+    let max = parseFloat(wallet.amount);
+    if (amount >= max + order.adjusted) {
+      res.json({ status: "fail", message: "Invalid Amount" });
+      return;
+    }
+    order.adjusted += amount;
+    await order.save();
+    wallet.amount = parseFloat(wallet.amount) - amount;
+    await wallet.save();
+    res.json({ status: "success", data: "OK" });
+    return;
+  } else if (type == "remove") {
+    let wallet = await FutureWalletModel.findOne({ user_id: user_id });
+    let orderSize = order.open_price * order.amount * order.leverage;
+    let max = 0.0;
+    if (order.usedUSDT > orderSize) {
+      max = order.usedUSDT - orderSize;
+    }
+    max += order.pnl;
+
+    if (amount > max + order.adjusted) {
+      res.json({ status: "fail", message: "Invalid Amount" });
+      return;
     }
 
-    if (type == 'add') {
-        let wallet = await FutureWalletModel.findOne({ user_id: user_id });
-        let max = parseFloat(wallet.amount);
-        if (amount >= max) {
-            res.json({ status: "fail", message: "Invalid Amount" });
-            return;
-        }
-        order.adjusted += amount;
-        await order.save();
-        wallet.amount = parseFloat(wallet.amount) - amount;
-        await wallet.save();
-        res.json({ status: "success", data: "OK" });
-        return;
-    }
-
-    else if (type == 'remove') {
-        let wallet = await FutureWalletModel.findOne({ user_id: user_id });
-        let orderSize = (order.open_price * order.amount) * order.leverage;
-        let max = 0.0;
-        if (order.usedUSDT > orderSize) {
-            max = order.usedUSDT - orderSize;
-        }
-        max += order.pnl;
-
-        if (amount > max) {
-            res.json({ status: "fail", message: "Invalid Amount" });
-            return;
-        }
-
-        order.adjusted -= amount;
-        await order.save();
-        wallet.amount = parseFloat(wallet.amount) + amount;
-        await wallet.save();
-        res.json({ status: "success", data: "OK" });
-        return;
-    }
-
-}
+    order.adjusted -= amount;
+    await order.save();
+    wallet.amount = parseFloat(wallet.amount) + amount;
+    await wallet.save();
+    res.json({ status: "success", data: "OK" });
+    return;
+  }
+};
 
 module.exports = AdjustMargin;
