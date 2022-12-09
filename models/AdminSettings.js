c1e8a75a42b0cf5b701db9febd4b773ef5230612
Adeel Ahmed <adeel@lessgo.co>
2022-12-09 15:15:23 +0500

    help and support

--- models/AdminSettings.js
+++ models/AdminSettings.js
@@ -9,10 +9,20 @@ const aboutUsSubSchema = new mongoose.Schema(
   { _id: false }
 );
 
+const helpSubSchema = new mongoose.Schema(
+  {
+    helpCenter: { type: String, required: false },
+    feedback: { type: String, required: false },
+    support: { type: String, required: false },
+  },
+  { _id: false }
+);
+
 const AdminSettingsSchema = new mongoose.Schema({
   deleteReasons: { type: [String], default: [] },
   aboutUs: { type: aboutUsSubSchema },
   privacy: { type: String },
+  help: { type: helpSubSchema },
   createdAt: {
     type: Date,
     default: Date.now,
