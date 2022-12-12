e82baffcc52d37906d0f34d542fa325efb1fd28a
Adeel Ahmed <adeel@lessgo.co>
2022-12-12 12:55:39 +0500

    update

--- controllers/getLocation.js
+++ controllers/getLocation.js
@@ -2,17 +2,14 @@ const geoip = require("geoip-lite");
 const authFile = require("../auth");
 
 const getLocation = async (req, res) => {
-  const { method, apiKey, userId, data } = req.body;
+  const { apiKey } = req.body;
 
   if (!apiKey) return res.json({ status: "error", message: "Api key is null" });
   const apiKeyCheck = await authFile.apiKeyChecker(apiKey);
   if (!apiKeyCheck)
     return res.json({ status: "error", message: "Api key is wrong" });
 
-    console.log(req.ip)
-
-  const geo = geoip.lookup('182.188.156.192');
-
+  const geo = geoip.lookup(req.ip);
   return res.json({ status: "success", data: geo });
 };
 
