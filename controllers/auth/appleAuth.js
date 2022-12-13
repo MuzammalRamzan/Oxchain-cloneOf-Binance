7a02f40cf0926609462f481d9ed2ba1a69a3f97a
Adeel Ahmed <adeel@lessgo.co>
2022-12-13 17:04:57 +0500

    update google and apple auth responses

--- controllers/auth/appleAuth.js
+++ controllers/auth/appleAuth.js
@@ -2,11 +2,19 @@ const fs = require("fs");
 const AppleAuth = require("apple-auth");
 const jwt = require("jsonwebtoken");
 const UserModel = require("../../models/User");
-const path = require('path');
+const path = require("path");
 const { getToken } = require("../../auth");
 const RegisterMailModel = require("../../models/RegisterMail");
+const RegisterSMSModel = require("../../models/RegisterSMS");
+const SecurityKey = require("../../models/SecurityKey");
+const Device = require("../../models/Device");
+const UserRef = require("../../models/UserRef");
+const LoginLogs = require("../../models/LoginLogs");
+const OneStepWithdrawModel = require("../../models/OneStepWithdraw");
+const WithdrawalWhiteListModel = require("../../models/WithdrawalWhiteList");
+const utilities = require("../../utilities");
 
-const authKeyPath = path.join(__dirname, '../../AuthKey.p8');
+const authKeyPath = path.join(__dirname, "../../AuthKey.p8");
 
 const config = {
   client_id: "com.sample.oxhain.client",
@@ -23,24 +31,122 @@ const auth = new AppleAuth(
 );
 
 const appleAuth = async (req, res) => {
+  const {
+    authCode,
+    deviceName,
+    deviceType,
+    manufacturer,
+    ip,
+    deviceModel,
+    user_id,
+    city,
+  } = req.body;
   try {
-    const response = await auth.accessToken(req.body.authCode);
+    const response = await auth.accessToken(authCode);
     const data = jwt.decode(response.id_token);
-    const user = await UserModel.findOneAndUpdate({ email: data.email }, data, {
-        upsert: true,
+    
+    const user = await UserModel.findOne({ email: data.email }).lean();
+
+    if (user) {
+      // securityLevel
+      let securityLevel = 0;
+      const emailVerifyCheck = await RegisterMailModel.findOne({
+        email: user.email,
+        status: "1",
       });
+      const smsVerifyCheck = await RegisterSMSModel.findOne({
+        phone_number: user.phone_number,
+        status: "1",
+      });
+      const securityKey = await SecurityKey.find({
+        user_id: user_id,
+        status: 1,
+      }).lean();
+      if (securityKey) securityLevel = securityLevel + 1;
+      if (emailVerifyCheck && smsVerifyCheck) securityLevel = securityLevel + 1;
+      if (user.twofa) securityLevel = securityLevel + 1;
+
+      // device
+      let device = new Device({
+        user_id: user._id,
+        deviceName: deviceName ?? "null",
+        deviceType: deviceType ?? "null",
+        loginTime: Date.now(),
+        ip: ip ?? "null",
+        city: city ?? "null",
+      });
+      await device.save();
+      let device_id = device._id;
+      req.session.device_id = device_id;
+
+      // user ref
+      const userRef = await UserRef.findOne({
+        user_id: user._id,
+      }).lean();
+
+      // login logs
+      const newUserLog = new LoginLogs({
+        user_id: user["_id"],
+        ip: ip ?? "null",
+        deviceName: deviceName ?? "null",
+        manufacturer: manufacturer ?? "null",
+        model: deviceModel ?? "null",
+        status: "completed",
+      });
+      await newUserLog.save();
+
+      // response
+      const data = {
+        response: "success",
+        email: user.email,
+        twofa: user.twofa,
+        emailVerify: !!emailVerifyCheck,
+        smsVerify: !!smsVerifyCheck,
+        device_id: device_id,
+        status: user.status,
+        user_id: user._id,
+        ref_id: userRef.refCode,
+        securityLevel,
+        device_token: device_id,
+        token: getToken({ user: user_id }),
+        name: user.name,
+      };
+
+      // settings
+      const withdrawalWhiteList = await WithdrawalWhiteListModel.findOne({
+        user_id: user._id,
+      }).lean();
+      data.withdrawalWhiteList = !!withdrawalWhiteList?.status;
+      const oneStepWithdraw = await OneStepWithdrawModel.findOne({
+        user_id: user._id,
+      }).lean();
+      data.oneStepWithdraw = !!oneStepWithdraw?.status;
+
+      return res.json({ status: "success", data });
+      
+    } else {
+      const newUser = UserModel({
+        email: data.email,
+      });
+      await newUser.save();
+
+      const refCode = utilities.makeId(10);
+      const newRef = new UserRef({
+        user_id: newUser._id,
+        refCode: refCode,
+      });
+      await newRef.save();
+
       await RegisterMailModel.updateOne(
         { email: data.email },
         {
           email: data.email,
           status: "1",
         },
-        {
-          upsert: true,
-        }
+        { upsert: true }
       );
-      const authToken = getToken({ user: user._id });
-      return res.json({ status: "success", data: { user, token: authToken } });
+      return res.json({ status: "success", data: newUser });
+    }
   } catch (err) {
     return res.status(401).json({ status: "error", message: err });
   }
