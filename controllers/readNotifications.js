e26bd9f568ee7f78d0f2300162a9c2b6424ae7e8
Adeel Ahmed <adeel@lessgo.co>
2022-12-13 17:25:17 +0500

    update read notification api

--- controllers/readNotifications.js
+++ controllers/readNotifications.js
@@ -4,17 +4,24 @@ const authFile = require("../auth.js");
 const readNotifications = async (req, res) => {
   const apiKey = req.body.apiKey;
   const userId = req.body.userId;
+  const notificationId = req.body.notificationId;
 
   if (!apiKey) return res.json({ status: "error", message: "Api key is null" });
   const apiKeyCheck = await authFile.apiKeyChecker(apiKey);
   if (!apiKeyCheck)
     return res.json({ status: "error", message: "Api key is wrong" });
-  if (!userId) return res.json({ status: "error", message: "User Id is null" });
 
-  await NotificationsModel.updateMany(
-    { userId, hasRead: false },
-    { hasRead: true }
-  );
+  if (!userId && !notificationId)
+    return res.json({
+      status: "error",
+      message: "Please provide user id or notification id",
+    });
+
+  const query = { hasRead: false };
+  if (userId) query.userId = userId;
+  if (notificationId) query._id = notificationId;
+
+  await NotificationsModel.updateMany(query, { hasRead: true });
   return res.json({ status: "success", message: "marked as read!" });
 };
 
