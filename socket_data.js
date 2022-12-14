6def624e22b0335604204ce271c3201726200b09
furkandamar-hctbilisim <99255244+furkandamar-hctbilisim@users.noreply.github.com>
2022-12-14 17:45:58 +0300

    future pnl balance

--- socket_data.js
+++ socket_data.js
@@ -118,130 +118,6 @@ async function fillMarketPrices() {
   }
 }
 fillMarketPrices();
-async function main() {
-  console.log("start");
-  await Connection.connection();
-  console.log("DB Connect");
-  wss.on("connection", async (ws) => {
-    console.info("websocket connection open");
-    if (ws.readyState === ws.OPEN) {
-      ws.send(
-        JSON.stringify({
-          msg1: "WELCOME TO OXHAIN",
-        })
-      );
-      ws.on("message", async (data) => {
-        let json = JSON.parse(data);
-        /*
-                        GetBinanceData(ws, json.pair);
-                        if (json.user_id != null && json.user_id != 'undefined') {
-                           GetWallets(ws, json.user_id);
-                           GetOrders(ws, json.user_id, json.spot_order_type ?? '');
-                           GetMarginOrders(ws, json.user_id, json.margin_order_type, json.margin_order_method_type);
-                           GetMarginBalance(ws, json.user_id);
-                        }
-            */
-
-        if (json.page == "trade") {
-          GetBinanceData(ws, json.pair);
-          if (json.user_id != null && json.user_id != "undefined") {
-            GetWallets(ws, json.user_id);
-            GetOrders(ws, json.user_id, json.spot_order_type ?? "");
-            GetMarginOrders(
-              ws,
-              json.user_id,
-              json.margin_order_type,
-              json.margin_order_method_type
-            );
-            GetMarginBalance(ws, json.user_id);
-          }
-        } else if (json.page == "spot_assets") {
-          let coinList = await CoinList.find({});
-          let wallet = await Wallet.find({ user_id: json.user_id });
-          let totalBtcValue = 0.0;
-          let totalUsdValue = 0.0;
-
-          let btcPrice = 0.0;
-          var b_ws = new WebSocket("wss://stream.binance.com/stream");
-
-          const initSocketMessage = {
-            method: "SUBSCRIBE",
-            params: ["!ticker@arr"],
-            // params: ["!miniTicker@arr"],
-            id: 1,
-          };
-
-          b_ws.onopen = (event) => {
-            b_ws.send(JSON.stringify(initSocketMessage));
-          };
-
-          // Reconnect connection when disconnect connection
-          b_ws.onclose = () => {
-            b_ws.send(JSON.stringify(initSocketMessage));
-          };
-          let walletData = {};
-          wallet.forEach(async (key, value) => {
-            let getCoinInfo = coinList.filter((x) => x._id == key.coin_id)[0];
-            walletData[getCoinInfo.symbol] = {
-              symbol: getCoinInfo.symbol,
-              name: getCoinInfo.name,
-              network: getCoinInfo.network,
-              icon: getCoinInfo.image_url,
-              balance: 0.0,
-              amount: key.amount,
-            };
-          });
-
-          b_ws.onmessage = function (event) {
-            const data = JSON.parse(event.data).data;
-            if (data != null && data != "undefined") {
-              totalUsdValue = 0.0;
-              totalBtcValue = 0.0;
-              for (var value in walletData) {
-                let getBtc = data.filter((x) => x.s == "BTCUSDT");
-                if (getBtc.length > 0) {
-                  btcPrice = getBtc[0].a;
-                }
-                console.log(value);
-                if (value == "Margin") {
-                  walletData[value].balance = walletData[value].amount;
-                  totalUsdValue += walletData[value].amount;
-                } else if (value == "USDT") {
-                  walletData[value].balance = walletData[value].amount;
-                  totalUsdValue += walletData[value].amount;
-                } else {
-                  let item = data.filter((x) => x.s == value + "USDT");
-                  if (item.length > 0) {
-                    walletData[value].balance =
-                      parseFloat(walletData[value].amount) * item[0].a;
-                    totalUsdValue +=
-                      parseFloat(walletData[value].amount) * item[0].a;
-                  }
-                }
-              }
-              totalBtcValue = totalUsdValue / btcPrice;
-              ws.send(
-                JSON.stringify({
-                  type: "balances",
-                  content: {
-                    wallets: walletData,
-                    totalUSD: totalUsdValue,
-                    totalBTC: totalBtcValue,
-                  },
-                })
-              );
-              console.log(walletData);
-            }
-          };
-        } else if (json.page == "global") {
-        } else if (json.page == "devices") {
-          GetDeviceStatus(ws, json.user_id);
-        } else if (json.page == "logout_device") {
-        }
-      });
-    }
-  });
-}
 
 async function test() {
   await Connection.connection();
@@ -638,7 +514,7 @@ async function CalculateFutureBalance(user_id) {
         user_id: user_id,
         status: 0,
         method: "market",
-        margin_type: "cross",
+        future_type: "cross",
       },
     },
 
@@ -651,9 +527,11 @@ async function CalculateFutureBalance(user_id) {
     },
   ]);
   let wallet = await FutureWalletModel.findOne({ user_id: user_id }).exec();
+  console.log(getOpenOrders.length);
   if (getOpenOrders.length == 0) return wallet.amount;
   if (wallet == null) return 0;
   let balance = wallet.amount + getOpenOrders[0].total;
+  console.log(balance, " | ", wallet.amount," | ", getOpenOrders[0].total);
   if (balance < 0) return 0;
   return balance;
 }
