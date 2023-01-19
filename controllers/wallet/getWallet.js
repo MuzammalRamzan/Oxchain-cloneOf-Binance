8f01aa224878a49b66ac8b8eb02003d4c8926631
volkansaka <volkansaka1@hotmail.com>
2023-01-19 18:02:22 +0300

    API Management Integrated to getWallet

--- controllers/wallet/getWallet.js
+++ controllers/wallet/getWallet.js
@@ -3,44 +3,73 @@ const Pairs = require("../../models/Pairs");
 var authFile = require("../../auth.js");
 const CoinList = require("../../models/CoinList");
 
-const getWallet = async function (req, res) {
-  var api_key_result = req.body.api_key;
-  var result = await authFile.apiKeyChecker(api_key_result);
+const ApiKeysModel = require("../../models/ApiKeys");
+const ApiRequest = require("../../models/ApiRequests");
 
-  if (result === true) {
-    var _wallets = await Wallet.find({
-      user_id: req.body.user_id
+const getWallet = async function (req, res) {
+
+
+
+  var api_key_result = req.body.api_key;
+
+  let api_result = await authFile.apiKeyChecker(api_key_result);
+  let apiRequest = "";
+
+  if (api_result === false) {
+    let checkApiKeys = "";
+
+    checkApiKeys = await ApiKeysModel.findOne({
+      api_key: api_key_result,
+      get_balance: "1"
     }).exec();
 
-    console.log(_wallets);
-    var wallets = new Array();
-
-    for (var i = 0; i < _wallets.length; i++) {
-
-      let item = _wallets[i];
-
-      let coinInfo = await CoinList.findOne({ _id: item.coin_id }).exec();
-
-      if (coinInfo == null) continue;
-
-      if (coinInfo.name == "Margin") continue;
-
-      wallets.push({
-        id: item._id,
-        coin_id: item.coin_id,
-        balance: item.amount,
-        symbolName: coinInfo.symbol + "/USDT",
-        symbol: coinInfo.symbol,
-        name: coinInfo.name,
-        icon: coinInfo.image_url,
+    if (checkApiKeys != null) {
+      apiRequest = new ApiRequest({
+        api_key: api_key_result,
+        request: "getBalance",
+        ip: req.body.ip ?? req.connection.remoteAddress,
+        user_id: checkApiKeys.user_id,
       });
+      await apiRequest.save();
     }
-
-    res.json({ status: "success", data: wallets });
-
-  } else {
-    res.json({ status: "fail", message: "not_found" });
+    else {
+      res.json({ status: "fail", message: "Forbidden 403" });
+      return;
+    }
   }
+
+  var _wallets = await Wallet.find({
+    user_id: req.body.user_id
+  }).exec();
+
+  console.log(_wallets);
+  var wallets = new Array();
+
+  for (var i = 0; i < _wallets.length; i++) {
+
+    let item = _wallets[i];
+
+    let coinInfo = await CoinList.findOne({ _id: item.coin_id }).exec();
+
+    if (coinInfo == null) continue;
+
+    if (coinInfo.name == "Margin") continue;
+
+    wallets.push({
+      id: item._id,
+      coin_id: item.coin_id,
+      balance: item.amount,
+      symbolName: coinInfo.symbol + "/USDT",
+      symbol: coinInfo.symbol,
+      name: coinInfo.name,
+      icon: coinInfo.image_url,
+    });
+  }
+
+  res.json({ status: "success", data: wallets });
+
+
+
 };
 
 module.exports = getWallet;
