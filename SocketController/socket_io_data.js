c715948c96d7a36e5e62901f885702bf8cbb7bff
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2023-01-20 16:23:28 +0300

    spot assets

--- SocketController/socket_io_data.js
+++ SocketController/socket_io_data.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const https = require("https");
+const WebSocket = require("ws");
+const WebSocketServer = require("ws").Server;
 const socketio = require("socket.io");
 var fs = require('fs');
 const CheckLogoutDevice = require("./device/check_logout_device");
@@ -29,6 +31,7 @@ const FutureTradeHistory = require("./trade/future/trade_history");
 const FutureTransactionHistory = require("./trade/future/transaction_history");
 const FutureAssets = require("./trade/future/future_funds");
 const GetSpotWallet = require("./wallet/getSpotWallet");
+const CoinList = require("../models/CoinList");
 
 require("dotenv").config();
 
@@ -167,13 +170,48 @@ io.on("connection", async (socket) => {
 
 });
 
+global.MarketData = {};
+async function fillMarketPrices() {
+    
+    let coinList = await CoinList.find({});
+        
+        var b_ws = new WebSocket("wss://stream.binance.com/stream");
+        for (var k = 0; k < coinList.length; k++) {
+            global.MarketData[coinList[k].symbol + "USDT"] = { bid: 0.0, ask: 0.0 };
+        }
 
+        const initSocketMessage = {
+            method: "SUBSCRIBE",
+            params: ["!ticker@arr"],
+            // params: ["!miniTicker@arr"],
+            id: 1,
+        };
+
+        b_ws.onopen = (event) => {
+            b_ws.send(JSON.stringify(initSocketMessage));
+        };
+
+        // Reconnect connection when disconnect connection
+        b_ws.onclose = () => {
+            b_ws.send(JSON.stringify(initSocketMessage));
+        };
+        b_ws.onmessage = function (event) {
+            const data = JSON.parse(event.data).data;
+            if (data != null && data != "undefined") {
+                for (var m = 0; m < data.length; m++) {
+                    let x = data[m];
+                    global.MarketData[x.s] = { bid: x.b, ask: x.a };
+                }
+            }
+        };
+}
+
+fillMarketPrices();
 
 function checkRoomOrJoin(socket, id) {
     if (socket.rooms.has(id) == false) {
         socket.join(id);
     }
-    console.log(socket.rooms);
 }
 
 
