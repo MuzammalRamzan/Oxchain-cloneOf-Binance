9deefdfcc716d088dab80372c9cadb2dcbc1df78
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2023-01-20 17:24:40 +0300

    withdraw disable pin

--- controllers/withdraw/withdraw.js
+++ controllers/withdraw/withdraw.js
@@ -160,6 +160,7 @@ const withdraw = async (req, res) => {
     let check1 = "";
     let check3 = "";
 
+    /*
     if (email != undefined && email != null && email != "") {
       check1 = await MailVerification.findOne({
         user_id: user_id,
@@ -176,7 +177,8 @@ const withdraw = async (req, res) => {
         });
 
     }
-
+    */
+/*
     if (phone != undefined && phone != null && phone != "") {
       check3 = await SMSVerification.findOne
         ({
@@ -194,6 +196,7 @@ const withdraw = async (req, res) => {
         });
     }
 
+    */
     if (check1 != "") {
       check1.status = 1;
       check1.save();
