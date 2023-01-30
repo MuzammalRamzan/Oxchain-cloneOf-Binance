638d584c308f041c80e6e3c7caa003b82d4bf85d
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2023-01-30 10:44:51 +0300

    campus program twitter cron completed

--- CronController/socialmedia/twitterPostCheckCron.js
+++ CronController/socialmedia/twitterPostCheckCron.js
@@ -25,12 +25,41 @@ const TwitterPostCheckCron = async () => {
             let item = tweets.data.data[j];
 
             if (item.text.indexOf('Oxhain') != -1) {
+                console.log(item);
                 let check = await SocialMediaPostModel.findOne({ post_link: item.id });
                 if (check == null) {
                     let save = new SocialMediaPostModel({ post_link: item.id, user_id: program[i].user_id, platform: "Twitter" });
                     await save.save();
                 }
             }
+
+        }
+
+        //Check last 7 day
+        let getLastPost = await SocialMediaPostModel.findOne({ user_id: program[i].user_id, platform: "Twitter" }, {}, { sort: { createdAt: -1 } });
+        let now = new Date();
+        let lastItemDate = new Date(getLastPost.createdAt);
+        console.log(lastItemDate)
+        console.log(now)
+        console.log(lastItemDate.getTime());
+        console.log(now.getTime());
+        let diffrentDays = Math.round(Math.abs(now.getTime() - lastItemDate.getTime()) / (1000 * 60 * 60 * 24));
+        if (diffrentDays > 7) {
+            let lastCaveat = program[i].lastCaveat;
+            if (lastCaveat != null) {
+                let lastCaveatDate = new Date(lastCaveat);
+                let caveatDiffrentDays = Math.round(Math.abs(now.getTime() - lastCaveatDate.getTime()) / (1000 * 60 * 60 * 24));
+                if (caveatDiffrentDays >= 1) {
+                    program[i].caveat += 1;
+                    program[i].lastCaveat = Date.now();
+                    await program[i].save();
+                }
+            } else {
+                program[i].caveat = 1;
+                program[i].lastCaveat = Date.now();
+                await program[i].save();
+            }
+
 
         }
     }
--- models/IBModel.js
+++ models/IBModel.js
@@ -2,7 +2,13 @@ const { default: mongoose } = require("mongoose");
 
 const IBModel = mongoose.Schema({
     user_id : {type : mongoose.Schema.Types.ObjectId, ref : "User"},
-    status : {type : Number, default: 0}
+    status : {type : Number, default: 0},
+    caveat: { type: Number, default: 0 },
+    lastCaveat: { type: Date },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model("IBModel", IBModel);
