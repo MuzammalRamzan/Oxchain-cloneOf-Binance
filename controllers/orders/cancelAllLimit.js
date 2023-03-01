3d1c747740b56cdf4d84c5ea986a3842a2e1081a
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2023-03-01 12:32:12 +0300

    delete all limit completed

--- controllers/orders/cancelAllLimit.js
+++ controllers/orders/cancelAllLimit.js
@@ -1,37 +1,79 @@
 const Wallet = require("../../models/Wallet");
 const Pairs = require("../../models/Pairs");
-const Orders = require("../../models/Orders");
+
 const MarginOrder = require("../../models/MarginOrder");
+const Orders = require("../../models/Orders");
 
 const cancelAllLimit = async function (req, res) {
-  
+
+
   var user_id = req.body.user_id;
-  let orders = await Orders.findAll({
-    _id: req.body.order_id,
-    user_id: req.body.user_id,
-    type: "limit",
+  if (user_id == null || user_id == '')
+    return res.json({ status: 'fail', message: "user not found" });
+  let orders = await Orders.find({
+    user_id: user_id,
+    status: 1,
+    $or: [
+      {
+        type: "limit",
+      },
+      {
+        type: "stop_limit",
+      }
+    ]
+
   }).exec();
   for (var i = 0; i < orders.length; i++) {
     let order = orders[i];
-    let amount = order.amount;
-    let pair = await Pairs.findOne({ symbolOneID: order.pair_id }).exec();
+    let wallet = null;
 
-    if (order.method == "buy") {
-      var toWallet = await Wallet.findOne({
-        coin_id: pair.symbolTwoID,
-        user_id: order.user_id,
-      }).exec();
-      toWallet.amount = toWallet.amount + order.target_price * amount;
-      await toWallet.save();
-    } else {
-      var toWallet = await Wallet.findOne({
-        coin_id: pair.symbolOneID,
-        user_id: order.user_id,
-      }).exec();
-      toWallet.amount = toWallet.amount + amount;
-      await toWallet.save();
+    try {
+      let amount = parseFloat(order.amount);
+      if (amount == null || amount == '') {
+        order.status = 2;
+        await order.save();
+        continue;
+      }
+      let pair = await Pairs.findOne({ symbolOneID: order.pair_id }).exec();
+
+      if (order.method == "buy") {
+        wallet = await Wallet.findOne({
+          coin_id: pair.symbolTwoID,
+          user_id: order.user_id,
+        });
+        if (wallet == null) {
+          order.status = 2;
+          await order.save();
+          continue;
+        }
+        wallet.amount = parseFloat(wallet.amount) + (parseFloat(order.target_price) * amount);
+        await wallet.save();
+      } else {
+        wallet = await Wallet.findOne({
+          coin_id: pair.symbolOneID,
+          user_id: order.user_id,
+        });
+        if (wallet == null) {
+          order.status = 2;
+          await order.save();
+          continue;
+        }
+        wallet.amount = parseFloat(wallet.amount) + parseFloat(amount);
+        await wallet.save();
+        order.status = 2;
+        await order.save();
+
+      }
+    } catch (err) {
+      console.log(order);
+      console.log(wallet);
+      console.log(err.message);
+      return res.json({ status: 'fail', message: err.message });
+      return;
+
     }
   }
+  return res.json({ status: 'success', data: 'ok' });
 };
 
 module.exports = cancelAllLimit;
