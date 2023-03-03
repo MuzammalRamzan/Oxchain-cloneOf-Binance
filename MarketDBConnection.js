2f39cc53b8910ed7ae2874a694668353dde46204
Furkan Damar <furkandamar@Furkans-MacBook-Air.local>
2023-03-03 15:35:29 +0300

    hide log

--- MarketDBConnection.js
+++ MarketDBConnection.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 async function MarketDBConnection() {
-    console.log("mongodb://" +
-    process.env.MARKET_DB_UID
-    + ":" +
-    process.env.MARKET_DB_PASS
-    + "@" + process.env.MARKETDBIP + "/?retryWrites=true&w=majority");
+   
     await mongoose.connect("mongodb://" +
         process.env.MARKET_DB_UID
         + ":" +
