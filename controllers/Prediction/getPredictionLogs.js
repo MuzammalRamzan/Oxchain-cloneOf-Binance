878dd738558c8b36ef132fe60e1db13324cd0b64
Muzammil Ramzan <m.muzammal208@gmail.com>
2023-03-07 13:30:05 +0500

    updated getPredictionLogs api to filter data according to time

--- controllers/Prediction/getPredictionLogs.js
+++ controllers/Prediction/getPredictionLogs.js
@@ -2,122 +2,114 @@ const Prediction = require('../../models/Prediction');
 const PredictionHistory = require('../../models/PredictionHistory');
 
 const log = async (req, res) => {
+	let timeInMs;
+	let thirtyDaysAgo = new Date();
+	thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+	if (req.body.interval === '5m') {
+		timeInMs = 5 * 60 * 1000; // 5 minutes in milliseconds
+	} else if (req.body.interval === '1h') {
+		timeInMs = 60 * 60 * 1000; // 1 hour in milliseconds
+	} else {
+		// handle invalid interval value
+	}
+	let coinCheck = await Prediction.find({}).sort({ createdAt: -1 });
 
+	//get different coin symbols from prediction history
+	let coinSymbols = [];
+	for (let i = 0; i < coinCheck.length; i++) {
+		let data = coinCheck[i];
+		if (!coinSymbols.includes(data.coin_symbol)) {
+			coinSymbols.push(data.coin_symbol);
+		}
+	}
 
+	console.log(coinSymbols);
 
+	let generalDataArray = [];
+	for (let i = 0; i < coinSymbols.length; i++) {
+		let foreachPredictionData = [];
+		let predictionHistoryData = await PredictionHistory.find({
+			coin_symbol: coinSymbols[i],
+			createdAt: { $gte: thirtyDaysAgo },
+			interval: req.body.interval,
+		})
+			.sort({ createdAt: -1 })
+			.limit(4);
 
+		let predictionData = await Prediction.find({
+			coin_symbol: coinSymbols[i],
+		})
+			.sort({ createdAt: -1 })
+			.limit(1);
 
-    let coinCheck = await Prediction.find({
-    }).sort({ createdAt: -1 });
+		for (let i = 0; i < predictionHistoryData.length - 1; i++) {
+			let data = predictionHistoryData[i];
+			let startTime = new Date(data.createdAt).toLocaleTimeString();
+			let endTimeStamp = new Date(data.createdAt).getTime() + timeInMs;
+			let endTime = new Date(endTimeStamp).toLocaleTimeString();
+			let timeInterval = `${startTime}-${endTime}`;
+			let nextData = predictionHistoryData[i + 1];
 
+			if (data.prediction == 1) {
+				if (data.price < nextData.price) {
+					foreachPredictionData.push({
+						prediction: data.prediction,
+						price: nextData.price,
+						timeInterval,
+						isSuccess: false,
+					});
+				} else {
+					foreachPredictionData.push({
+						prediction: data.prediction,
+						price: nextData.price,
+						timeInterval,
+						isSuccess: true,
+					});
+				}
+			}
 
-    //get different coin symbols from prediction history
-    let coinSymbols = [];
-    for (let i = 0; i < coinCheck.length; i++) {
-        let data = coinCheck[i];
-        if (!coinSymbols.includes(data.coin_symbol)) {
-            coinSymbols.push(data.coin_symbol);
-        }
-    }
+			if (data.prediction == 0) {
+				if (data.price > nextData.price) {
+					foreachPredictionData.push({
+						prediction: data.prediction,
+						isSuccess: false,
+						price: nextData.price,
+						timeInterval,
+					});
+				} else {
+					foreachPredictionData.push({
+						prediction: data.prediction,
+						isSuccess: true,
+						price: nextData.price,
+						timeInterval,
+					});
+				}
+			}
+		}
+		let pendingStartTime = new Date(
+			predictionData[0].createdAt
+		).toLocaleTimeString();
+		let pendingEndTimestamp =
+			new Date(predictionData[0].createdAt).getTime() + timeInMs;
+		let pendingEndTime = new Date(pendingEndTimestamp).toLocaleTimeString();
+		let interval = `${pendingStartTime}-${pendingEndTime}`;
+		foreachPredictionData.push({
+			prediction: predictionData[0].prediction,
+			price: predictionData[0].price,
+			interval,
+			isSuccess: 'pending',
+		});
 
-    console.log(coinSymbols);
+		generalDataArray.push({
+			coin_symbol: coinSymbols[i],
+			data: foreachPredictionData,
+		});
+	}
 
-
-    let generalDataArray = [];
-    for (let i = 0; i < coinSymbols.length; i++) {
-        let foreachPredictionData = [];
-        let predictionHistoryData = await PredictionHistory.find({
-            coin_symbol: coinSymbols[i],
-        }).sort({ createdAt: -1 }).limit(4);
-
-        let predictionData = await Prediction.find({
-            coin_symbol: coinSymbols[i],
-        }).sort({ createdAt: -1 }).limit(1);
-
-
-        console.log(predictionHistoryData.length);
-
-
-        for (let i = 0; i < predictionHistoryData.length - 1; i++) {
-
-            let data = predictionHistoryData[i];
-
-            let nextData = predictionHistoryData[i + 1];
-
-            if (data.prediction == 1) {
-
-                if (data.price < nextData.price) {
-                    foreachPredictionData.push(
-                        {
-                            prediction: data.prediction,
-                            price: nextData.price,
-                            isSuccess: false,
-                        }
-                    )
-                }
-                else {
-                    foreachPredictionData.push(
-                        {
-                            prediction: data.prediction,
-                            price: nextData.price,
-                            isSuccess: true,
-                        }
-                    )
-                }
-            }
-
-            if (data.prediction == 0) {
-
-                if (data.price > nextData.price) {
-                    foreachPredictionData.push(
-                        {
-                            prediction: data.prediction,
-                            isSuccess: false,
-                            price: nextData.price,
-                        }
-                    )
-                }
-                else {
-                    foreachPredictionData.push(
-                        {
-                            prediction: data.prediction,
-                            isSuccess: true,
-                            price: nextData.price,
-                        }
-                    )
-                }
-            }
-
-        }
-        foreachPredictionData.push(
-            {
-                prediction: predictionData[0].prediction,
-                price: predictionData[0].price,
-                isSuccess: "pending",
-            });
-
-
-        generalDataArray.push(
-            {
-                coin_symbol: coinSymbols[i],
-                data: foreachPredictionData,
-            }
-        );
-
-    }
-
-    return res.json({
-        status: "success",
-        data: generalDataArray,
-    });
-
-
-
-
-
-
-
-
-}
+	return res.json({
+		status: 'success',
+		data: generalDataArray,
+	});
+};
 
 module.exports = log;
