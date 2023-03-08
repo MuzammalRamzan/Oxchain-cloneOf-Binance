ce81009c66085ca5ea3781c1071ae5f4f2e6d1e8
Muzammil Ramzan <m.muzammal208@gmail.com>
2023-03-08 16:04:48 +0500

    updated code for add prediction

--- controllers/Prediction/addPrediction.js
+++ controllers/Prediction/addPrediction.js
@@ -12,127 +12,101 @@ let axios = require('axios');
 const AITradeLogsModel = require('../../models/AITradeLogs');
 const PredictionHistory = require('../../models/PredictionHistory');
 
-
-
-
 const addPrediction = async (req, res) => {
+	const { symbol, prediction, api_key, priceData, interval } = req.body;
 
-    const { symbol, prediction, api_key, priceData, interval } = req.body;
+	const result = await authFile.apiKeyChecker(api_key);
 
-    const result = await authFile.apiKeyChecker(api_key);
+	if (result === true) {
+		let predicationHistory = await PredictionHistoryModel.findOne({
+			coin_symbol: symbol,
+			interval: interval,
+		})
+			.sort({ createdAt: -1 })
+			.exec();
 
-    if (result === true) {
+		let isSuccess;
+		if (predicationHistory) {
+			if (predicationHistory.prediction == 1) {
+				if (priceData > predicationHistory.price) {
+					isSuccess = 1;
+				} else {
+					isSuccess = 0;
+				}
+			}
+			if (predicationHistory.prediction == 0) {
+				if (priceData < predicationHistory.price) {
+					isSuccess = 1;
+				} else {
+					isSuccess = 0;
+				}
+			}
+			predicationHistory.isSuccess = isSuccess;
+			predicationHistory.save();
+		} else {
+			let newPredictionHistory = new PredictionHistoryModel({
+				coin_symbol: symbol,
+				prediction: prediction,
+				price: priceData,
+				interval: interval,
+				isSuccess: 'pending',
+			});
+		}
 
-        let predicationHistory = await PredictionHistoryModel.findOne({
-            coin_symbol: symbol,
-            interval: interval,
-        }).sort({ createdAt: -1}).exec();
+		let predictionCheck = await PredictionModel.findOne({
+			coin_symbol: symbol,
+			interval: interval,
+		}).exec();
 
-        let isSuccess;
-        if (predicationHistory) {
-            if(predicationHistory.prediction == 1){
-                if(priceData > predicationHistory.price){
-                    isSuccess = 1;
-                }else{
-                    isSuccess = 0;
-                }
-            }
-            if(predicationHistory.prediction == 0){
-                if(priceData < predicationHistory.price){
-                    isSuccess = 1;
-                }else{
-                    isSuccess = 0;
-                }
-            }
-            predicationHistory.isSuccess = isSuccess;
-            predicationHistory.save();
-        }else{
-            let newPredictionHistory = new PredictionHistoryModel({
-                coin_symbol: symbol,
-                prediction: prediction,
-                price: priceData,
-                interval: interval,
-                isSuccess:"pending",
-            });
-        }
-        return res.json({
-            status: "success",
-            message: "Prediction added",
-            showableMessage: "Prediction added",
-        });
+		let newPredictionHistory = new PredictionHistoryModel({
+			coin_symbol: symbol,
+			prediction: prediction,
+			price: priceData,
+			interval: interval,
+		});
 
-        
-        let predictionCheck = await PredictionModel.findOne({
-            coin_symbol: symbol,
-            interval: interval
-        }).exec();
+		await newPredictionHistory.save();
 
-        let newPredictionHistory = new PredictionHistoryModel({
-            coin_symbol: symbol,
-            prediction: prediction,
-            price: priceData,
-            interval: interval
-        });
+		if (predictionCheck) {
+			predictionCheck.prediction = prediction;
+			predictionCheck.price = priceData;
+			await predictionCheck.save();
+		} else {
+			let newPrediction = new PredictionModel({
+				coin_symbol: symbol,
+				prediction: prediction,
+				price: priceData,
+				interval: interval,
+			});
+			await newPrediction.save();
+		}
 
-        await newPredictionHistory.save();
+		const pairs = await PairModel.findOne({
+			name: symbol + '/USDT',
+		});
 
-        if (predictionCheck) {
-            predictionCheck.prediction = prediction;
-            predictionCheck.price = priceData;
-            await predictionCheck.save();
-        }
-        else {
-            let newPrediction = new PredictionModel({
-                coin_symbol: symbol,
-                prediction: prediction,
-                price: priceData,
-                interval: interval
-            });
-            await newPrediction.save();
-        }
+		console.log(pairs);
+		if (pairs) {
+			let AIFutureModelCheck = await AIFutureModel.findOne({
+				pair: symbol,
+			}).exec();
 
+			if (AIFutureModelCheck) {
+				//3 method, biri otomatik (5 dk da bir veya 1 saatte bir pozisyonu kapatıp yenisini açar)
+				//2. si manuel, emir açılır, verilen emirde max Order sayısı alınır, max order sayısı kadar emir oluşturulur, emirler kullanıcı isteyene kadar kapanmaz
+				//3. sü tp/sl
 
-        const pairs = await PairModel.findOne({
-            name: symbol + "/USDT"
-        });
+				console.log(AIFutureModelCheck);
+				for (let i = 0; i < AIFutureModelCheck.length; i++) {}
+			}
+		}
 
-        console.log(pairs);
-        if (pairs) {
-
-            let AIFutureModelCheck = await AIFutureModel.findOne({
-                pair: symbol,
-            }).exec();
-
-            if (AIFutureModelCheck) {
-
-
-                //3 method, biri otomatik (5 dk da bir veya 1 saatte bir pozisyonu kapatıp yenisini açar)
-                //2. si manuel, emir açılır, verilen emirde max Order sayısı alınır, max order sayısı kadar emir oluşturulur, emirler kullanıcı isteyene kadar kapanmaz
-                //3. sü tp/sl 
-
-                
-console.log(AIFutureModelCheck);
-                for (let i = 0; i < AIFutureModelCheck.length; i++) 
-                {
-
-                }
-
-            }
-
-
-
-        }
-
-
-        return res.json({
-            status: "success",
-            message: "Prediction added",
-            showableMessage: "Prediction added",
-        });
-
-    }
-
-
-}
+		return res.json({
+			status: 'success',
+			message: 'Prediction added',
+			showableMessage: 'Prediction added',
+		});
+	}
+};
 
 module.exports = addPrediction;
