4d5ab2afe90c723f5011f5ff554153173830fb72
awais <awaisliaqat5@gmail.com>
2023-03-08 19:38:27 +0500

    getPredicationLogFinal

--- controllers/Prediction/getPredictionLogs.js
+++ controllers/Prediction/getPredictionLogs.js
@@ -82,145 +82,21 @@ const log = async (req, res) => {
 					data: predictionHistoryData.sort(),
 				});
 			}else{
-				return res.status(200).json({
+				return res.status(404).json({
 					status: 'fail',
 					message: 'data not found',
 					showableMessage: 'No predication available for this coin',
 				});
 			}
 		}
-
-		return res.status(200).json(predictionDataResponse);
-		let generalDataArray = [];
-		for (let i = 0; i < coinSymbols.length; i++) {
-			let foreachPredictionData = [];
-			let predictionHistoryData = await PredictionHistory.find({
-				coin_symbol: coinSymbols[i],
-				createdAt: { $gte: thirtyDaysAgo },
-				interval: req.body.interval,
-			})
-				.sort({ createdAt: -1 })
-				.limit(4);
-			let noOfSuccessfullPredictions = await PredictionHistory.countDocuments({
-				coin_symbol: coinSymbols[i],
-				createdAt: { $gte: thirtyDaysAgo },
-				interval: req.body.interval,
-				prediction: 1,
-			})
-				.sort({ createdAt: -1 })
-				.limit(4);
-			console.log(noOfSuccessfullPredictions);
-
-			let predictionData = await Prediction.find({
-				coin_symbol: coinSymbols[i],
-			})
-				.sort({ createdAt: -1 })
-				.limit(1);
-
-			for (let i = 0; i < predictionHistoryData.length - 1; i++) {
-				let data = predictionHistoryData[i];
-				let startTime = new Date(data.createdAt).toLocaleTimeString([], {
-					hour12: false,
-					hour: '2-digit',
-					minute: '2-digit',
-				});
-				let endTimeStamp = new Date(data.createdAt).getTime() + timeInMs;
-				let endTime = new Date(endTimeStamp).toLocaleTimeString([], {
-					hour12: false,
-					hour: '2-digit',
-					minute: '2-digit',
-				});
-				let timeInterval = `${startTime}-${endTime}`;
-				let nextData = predictionHistoryData[i + 1];
-				// console.log('coinInfo', coinInfo.symbol);
-				if (data.prediction == 1) {
-					if (data.price < nextData.price) {
-						foreachPredictionData.push({
-							prediction: data.prediction,
-							price: nextData.price,
-							timeInterval,
-							isSuccess: false,
-						});
-					} else {
-						foreachPredictionData.push({
-							prediction: data.prediction,
-							price: nextData.price,
-							timeInterval,
-							isSuccess: true,
-						});
-					}
-				}
-
-				if (data.prediction == 0) {
-					if (data.price > nextData.price) {
-						foreachPredictionData.push({
-							prediction: data.prediction,
-							isSuccess: false,
-							price: nextData.price,
-							timeInterval,
-						});
-					} else {
-						foreachPredictionData.push({
-							prediction: data.prediction,
-							isSuccess: true,
-							price: nextData.price,
-							timeInterval,
-						});
-					}
-				}
-			}
-			let pendingStartTime = new Date(
-				predictionData[0].createdAt
-			).toLocaleTimeString([], {
-				hour12: false,
-				hour: '2-digit',
-				minute: '2-digit',
-			});
-			let pendingEndTimestamp =
-				new Date(predictionData[0].createdAt).getTime() + timeInMs;
-			let pendingEndTime = new Date(pendingEndTimestamp).toLocaleTimeString(
-				[],
-				{
-					hour12: false,
-					hour: '2-digit',
-					minute: '2-digit',
-				}
-			);
-			let interval = `${pendingStartTime}-${pendingEndTime}`;
-			foreachPredictionData.push({
-				prediction: predictionData[0].prediction,
-				price: predictionData[0].price,
-				interval,
-				isSuccess: 'pending',
-			});
-			if (coinSymbols[i] == 'XRP') continue;
-			let coinInfo = await CoinList.findOne({
-				symbol: coinSymbols[i],
-			});
-			let accuracy =
-				(noOfSuccessfullPredictions / predictionHistoryData.length) * 100;
-			if (!accuracy) {
-				accuracy = 0;
-			}
-
-			generalDataArray.push({
-				coin_symbol: coinSymbols[i],
-				coinUrl: coinInfo.image_url,
-				noOfSuccessfullPredictions,
-				totalPredictions: predictionHistoryData.length,
-				accuracy,
-				data: foreachPredictionData,
-			});
-		}
-
 		return res.json({
 			status: 'success',
-			data: generalDataArray,
+			data: predictionDataResponse,
 		});
 	} catch (error) {
 		// Log the error for debugging purposes
 		return res.status(500).json({
-			status: 'Failed',
+			status: 'fail',
 			message: 'Internal Server Error',
 			showableMessage: error.message,
 		});
