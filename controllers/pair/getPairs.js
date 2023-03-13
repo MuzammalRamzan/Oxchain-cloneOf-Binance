edb293e3e651ad0a0a9abbbaf366b42a3966d095
volkansaka <volkansaka1@hotmail.com>
2023-03-13 15:43:20 +0300

    up

--- controllers/pair/getPairs.js
+++ controllers/pair/getPairs.js
@@ -1,8 +1,44 @@
 const Pairs = require("../../models/Pairs");
+const CoinList = require("../../models/CoinList");
 
 const getPairs = async function (req, res) {
   var list = await Pairs.find({ status: 1 }).exec();
-  res.json({ status: "success", data: list });
+
+
+  let returnData = [];
+  for (let i = 0; i < list.length; i++) {
+    let coinOne = await CoinList.findOne({ _id: list[i].symbolOneID }).exec();
+    let coinTwo = await CoinList.findOne({ _id: list[i].symbolTwoID }).exec();
+
+    if (coinOne == null || coinTwo == null) {
+      continue;
+    }
+
+    let obj = {
+      id: list[i]._id,
+      name: list[i].name,
+      symbolOne: coinOne.symbol,
+      symbolTwo: coinTwo.symbol,
+      symbolOneID: coinOne._id,
+      symbolTwoID: coinTwo._id,
+      status: list[i].status,
+      icon: coinOne.image_url,
+      createdAt: list[i].createdAt,
+      spot_fee: list[i].spot_fee,
+      margin_fee: list[i].margin_fee,
+      future_fee: list[i].future_fee,
+      depositFee: list[i].depositFee,
+      withdrawFee: list[i].withdrawFee,
+      type: list[i].type,
+      digits: list[i].digits,
+      tradeFee: list[i].tradeFee,
+    };
+
+    returnData.push(obj);
+  }
+
+  res.json({ status: "success", data: returnData });
+
 };
 
 module.exports = getPairs;
