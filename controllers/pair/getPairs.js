5540ed1f223bdfdc3e65a4fe8071eabe6f7701d5
fukoapps <f.damar@hotmail.com>
2023-03-20 14:41:41 +0300

    remove getpairs api key

--- controllers/pair/getPairs.js
+++ controllers/pair/getPairs.js
@@ -4,39 +4,7 @@ var authFile = require("../../auth");
 
 const getPairs = async function (req, res) {
 
-
-  let api_key_result = req.body.api_key;
-
-  let result = await authFile.apiKeyChecker(api_key_result);
-
-  if (result === true) {
-
-    let key = req.headers["key"];
-
-    if (!key) {
-      return res.json({ status: "fail", message: "key_not_found" });
-    }
-
-    if (!req.body.device_id || !req.body.user_id) {
-      return res.json({ status: "fail", message: "invalid_params (key, user id, device_id)" });
-    }
-
-    let checkKey = await authFile.verifyKey(key, req.body.device_id, req.body.user_id);
-
-
-    if (checkKey === "expired") {
-      return res.json({ status: "fail", message: "key_expired" });
-    }
-
-    if (!checkKey) {
-      return res.json({ status: "fail", message: "invalid_key" });
-    }
-
-
     var list = await Pairs.find({ status: 1 }).exec();
-
-
-
     let returnData = [];
     for (let i = 0; i < list.length; i++) {
       let coinOne = await CoinList.findOne({ _id: list[i].symbolOneID }).exec();
@@ -71,10 +39,7 @@ const getPairs = async function (req, res) {
     }
 
     return res.json({ status: "success", data: returnData });
-  }
-  else {
-    return res.json({ status: "fail", message: "Forbidden 403" });
-  }
+ 
 };
 
 module.exports = getPairs;
