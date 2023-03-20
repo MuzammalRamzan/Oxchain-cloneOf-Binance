9b94bca202459a9ca6788b71d071512c2bce5be4
Muzammil Ramzan <m.muzammal208@gmail.com>
2023-03-20 18:37:27 +0500

    updated withdraw and deposit api

--- controllers/withdraw/getWithdrawHistory.js
+++ controllers/withdraw/getWithdrawHistory.js
@@ -1,89 +1,115 @@
-const CoinList = require("../../models/CoinList");
-const Network = require("../../models/Network");
-const Withdraw = require("../../models/Withdraw");
-var authFile = require("../../auth.js");
+const CoinList = require('../../models/CoinList');
+const Network = require('../../models/Network');
+const Withdraw = require('../../models/Withdraw');
+var authFile = require('../../auth.js');
 
 const GetWithdrawHistory = async (req, res) => {
+	const { coin, status, firstDate, endDate, type } = req.body;
+	let result = await authFile.apiKeyChecker(req.body.api_key);
+	if (result === false) {
+		return res.json({
+			status: 'fail',
+			message: '403 Forbidden',
+			showableMessage: 'Forbidden 403',
+		});
+	}
 
+	let key = req.headers['key'];
 
-    let result = await authFile.apiKeyChecker(req.body.api_key);
-    if (result === false) {
-        return res.json({ status: "fail", message: "403 Forbidden", showableMessage: "Forbidden 403" });
-    }
+	if (!key) {
+		return res.json({ status: 'fail', message: 'key_not_found' });
+	}
 
-    let key = req.headers["key"];
+	if (!req.body.device_id || !req.body.user_id) {
+		return res.json({
+			status: 'fail',
+			message: 'invalid_params (key, user id, device_id)',
+		});
+	}
 
-    if (!key) {
-        return res.json({ status: "fail", message: "key_not_found" });
-    }
+	let checkKey = await authFile.verifyKey(
+		key,
+		req.body.device_id,
+		req.body.user_id
+	);
 
-    if (!req.body.device_id || !req.body.user_id) {
-        return res.json({ status: "fail", message: "invalid_params (key, user id, device_id)" });
-    }
+	if (checkKey === 'expired') {
+		return res.json({ status: 'fail', message: 'key_expired' });
+	}
 
-    let checkKey = await authFile.verifyKey(key, req.body.device_id, req.body.user_id);
+	if (!checkKey) {
+		return res.json({ status: 'fail', message: 'invalid_key' });
+	}
 
+	let { user_id } = req.body;
 
-    if (checkKey === "expired") {
-        return res.json({ status: "fail", message: "key_expired" });
-    }
+	if (!user_id) {
+		return res.json({
+			status: 'fail',
+			message: 'user_id is required',
+		});
+	}
+	let coinInfo = await CoinList.findOne({
+		symbol: coin,
+	}).exec();
+	let query = {};
+	if (user_id) {
+		query.user_id = user_id;
+	}
+	if (coinInfo._id) {
+		query.coin_id = coinInfo._id;
+	}
+	if (status) {
+		query.status = status;
+	}
+	if (firstDate && endDate) {
+		query.createdAt = {
+			$gte: firstDate,
+			$lte: endDate,
+		};
+	}
+	if (type) {
+		query.type = type;
+	}
 
-    if (!checkKey) {
-        return res.json({ status: "fail", message: "invalid_key" });
-    }
+	let withdrawHistory = await Withdraw.find(query).exec();
 
+	let withdrawHistoryData = [];
 
-    let { user_id } = req.body;
+	for (let i = 0; i < withdrawHistory.length; i++) {
+		let coinInfo = await CoinList.findOne({
+			_id: withdrawHistory[i].coin_id,
+		}).exec();
+		let networkInfo = await Network.findOne({
+			_id: withdrawHistory[i].network_id,
+		}).exec();
+		if (coinInfo == null || networkInfo == null) continue;
 
-    if (!user_id) {
-        return res.json(
-            {
-                status: "fail",
-                message: "user_id is required",
+		withdrawHistoryData.push({
+			id: withdrawHistory[i]._id,
+			coin: {
+				id: withdrawHistory[i].coin_id,
+				name: coinInfo.symbol,
+			},
+			network: {
+				id: withdrawHistory[i].network_id,
+				name: networkInfo.symbol,
+			},
+			hash: withdrawHistory[i].tx_id,
+			fee: withdrawHistory[i].fee,
+			amount: withdrawHistory[i].amount,
+			fromAddress: withdrawHistory[i].fromAddress,
+			toAddress: withdrawHistory[i].address,
+			date: withdrawHistory[i].createdAt,
+			status: withdrawHistory[i].status,
+		});
+	}
 
-            }
-        );
-    }
-
-
-    let withdrawHistory = await Withdraw.find({ user_id: user_id }).exec();
-
-    let withdrawHistoryData = [];
-
-    for (let i = 0; i < withdrawHistory.length; i++) {
-
-        let coinInfo = await CoinList.findOne({ _id: withdrawHistory[i].coin_id }).exec();
-        let networkInfo = await Network.findOne({ _id: withdrawHistory[i].network_id }).exec();
-        if (coinInfo == null || networkInfo == null) continue;
-
-        withdrawHistoryData.push({
-            id: withdrawHistory[i]._id,
-            coin: {
-                id: withdrawHistory[i].coin_id,
-                name: coinInfo.symbol,
-            },
-            network: {
-                id: withdrawHistory[i].network_id,
-                name: networkInfo.symbol,
-            },
-            hash: withdrawHistory[i].tx_id,
-            fee: withdrawHistory[i].fee,
-            amount: withdrawHistory[i].amount,
-            fromAddress: withdrawHistory[i].fromAddress,
-            toAddress: withdrawHistory[i].address,
-            date: withdrawHistory[i].createdAt,
-            status: withdrawHistory[i].status,
-        });
-    }
-
-    return res.json(
-        {
-            status: "success",
-            message: "withdraw history",
-            data: withdrawHistoryData
-        })
-
-
-}
+	return res.json({
+		status: 'success',
+		message: 'withdraw history',
+		data: withdrawHistoryData,
+	});
+};
 
 module.exports = GetWithdrawHistory;
