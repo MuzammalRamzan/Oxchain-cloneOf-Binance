efb16e99e8e84f6f6c28ab8a51debd7ba787ace3
volkansaka <volkansaka1@hotmail.com>
2023-03-22 17:47:36 +0300

    up

--- Functions/Future/tradeHistoryFillTable.js
+++ Functions/Future/tradeHistoryFillTable.js
@@ -2,7 +2,7 @@ async function TradeHistoryFillTable(orders) {
     let assets = [];
     for (var i = 0; i < orders.length; i++) {
         let order = orders[i];
-        assets.push({ "symbol": order.pair_name, "side": order.type, "price": order.open_price, "quantity": order.usedUSDT, "role": "Taker", "realized_profit": order.pnl, "createdAt": order.createdAt });
+        assets.push({ "symbol": order.pair_name, "fee": order.fee, "side": order.type, "price": order.open_price, "quantity": order.usedUSDT, "role": "Taker", "realized_profit": order.pnl, "createdAt": order.createdAt });
     }
     return assets;
 }
