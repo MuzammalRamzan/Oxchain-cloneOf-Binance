e408a24048fb5c97cb870e9f063c2a769a873427
Muzammil Ramzan <m.muzammal208@gmail.com>
2023-03-24 14:17:46 +0500

    Revert "merge"
    This reverts commit 8c32016b233b8d3df080d2ac8c7086e9a17ec10c.

--- models/RankAwards.js
+++ models/RankAwards.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RankAwardSchema = new Schema({
-	level: { type: String, required: true },
-	imageUrl: { type: String, required: true },
-	value: { type: String, required: true },
+  level: { type: String, required: true },
+  imageUrl: { type: String, required: true }
 });
 
 const RankAward = mongoose.model('RankAward', RankAwardSchema);
