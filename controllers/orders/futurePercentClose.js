c3f774bef26d4954819d68d4659fac539860fe9e
volkansaka <volkansaka1@hotmail.com>
2023-03-25 04:36:09 +0300

    up

--- controllers/orders/futurePercentClose.js
+++ controllers/orders/futurePercentClose.js
@@ -62,6 +62,7 @@ const FuturePercentClose = async (req, res) => {
         wallet.amount = splitLengthNumber(parseFloat(wallet.amount) + (parseFloat(order.usedUSDT) + parseFloat(order.pnl)));
         await wallet.save();
         let leverage = order.leverage;
+        let type = order.type;
         const fee = (order.usedUSDT + order.pnl) * leverage * (type == 'buy' ? 0.03 : 0.06) / 100.0;
 
         //CREATE NEW ORDER AS MARKET AND OPPOSITE SIDE
