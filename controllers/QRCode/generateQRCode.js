dd137bc6274deead9ce359872c0f971680ff739b
awais <awaisliaqat5@gmail.com>
2023-03-28 00:02:26 +0500

    AuthQRLogin

--- controllers/QRCode/generateQRCode.js
+++ controllers/QRCode/generateQRCode.js
@@ -1,18 +1,27 @@
 const QRCode = require('qrcode');
+const QRCodes = require('../../models/QRCodes');
+const utilities = require('../../utilities');
 
 const generateQRCode = async (req, res) => {
 	try {
-		const { device_id, deviceName, deviceOS, ip } = req.body;
-		let data = {
-			device_id,
-			deviceName,
-			deviceOS,
-			ip,
-		};
-		const qrCodeData = `${JSON.stringify(data)}`;
+		const { device_id, deviceName, deviceOS, ip, deviceVersion, deviceType} = req.body;
+		let QRtoken = utilities.hashData((Math.random() + 1).toString(36)) + Date.now();
+		let newQRCode = new QRCodes({
+			deviceId: device_id,
+			deviceName: deviceName,
+			deviceType: deviceType,
+			deviceOs: deviceOS,
+			deviceVersion: deviceVersion,
+			ip: ip,
+			qrToken: QRtoken,
+		});
+		newQRCode.save();
 
+		let start = new Date();
+		const query ={ "createdAt": { "$lt": new Date(Date.now() - 86400000) }}
+    	await QRCodes.deleteMany(query);
 		// Generate the QR code
-		const qrCode = await QRCode.toDataURL(qrCodeData);
+		const qrCode = await QRCode.toDataURL(QRtoken);
 
 		// Return the QR code data
 		res.json({ qrCode });
---
+++ models/QRCodes.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const QRCodes = new mongoose.Schema({
+    deviceId: { type: String, required: false },
+    deviceName: { type: String, required: false },
+    deviceType: { type: String, required: false },
+    deviceOs: { type: String, required: false },
+    deviceVersion: { type: String, required: false },
+    ip: { type: String, required: true }, 
+    qrToken: {type: String, required: true},
+    status: {type: String, required: true, default:0},
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("QRCodes", QRCodes);
